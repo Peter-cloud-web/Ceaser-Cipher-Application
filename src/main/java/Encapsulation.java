40fa8363dfff5a7cc212798d630d1c5f8aeb1617
Peter-cloud-web <petergichia164@gmail.com>
2020-01-26 18:08:11 +0300

    Defining a constructor

--- src/main/java/Encapsulation.java
+++ src/main/java/Encapsulation.java
@@ -1,4 +1,9 @@
 public class Encapsulation {
     private int key;
     private String word;
+
+    public Encapsulation(int key,String word){
+        this.key = key;
+        this.word = word;
+    }
 }
