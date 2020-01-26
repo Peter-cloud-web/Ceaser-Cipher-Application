436d945311d5d16fbb9bb50ce4e6454826f2823e
Peter-cloud-web <petergichia164@gmail.com>
2020-01-26 17:52:40 +0300

    first test to decode a character

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecryptTest {
+    @Test
+    public void Decrypting_shiftAlphabet_String() {
+        Decrypt testDecryption = new Decrypt();
+        String expectedOutput = "b";
+        assertEquals(expectedOutput, testDecryption.decode("d", 2));
+    }
 
 }
