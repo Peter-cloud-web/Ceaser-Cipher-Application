74f7008751a37187deca7b3a630e901c005da461
Peter-cloud-web <petergichia164@gmail.com>
2020-01-26 17:42:14 +0300

    Second test to tests  if the program can encrypt a word

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -9,6 +9,10 @@ public class EncryptTest {
         String expectedOutput = "G";
         assertEquals(expectedOutput, testEncryption.encode("e", 2));
     }
+    @Test
+    public void Encrypting_shiftWord_String() {
+
+    }
 
 
 }
