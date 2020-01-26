18b3990c2e1922e471742cbcc2d1c68d9a41ebf4
Peter-cloud-web <petergichia164@gmail.com>
2020-01-26 17:37:07 +0300

    First test to test a character is encrypted

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,6 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptTest {
+    @Test
+    public void Encrypting_shiftAlphabet_String() {
+        Encrypt testEncryption = new Encrypt();
+        String expectedOutput = "G";
+        assertEquals(expectedOutput, testEncryption.encode("e", 2));
+    }
 
 
 }
